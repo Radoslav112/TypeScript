c12198dec5105c58359428af244e1ebbebc36713
Radoslav112 <Radi_boy@abv.bg>
2022-07-15 11:18:08 +0300

    delete dist folder

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const person_1 = require("./person");
-function newFunc() {
-    return "This is my new function message.";
-}
-console.log(newFunc());
-let p = new person_1.Person("Ivan", "Ivanov", 49);
-p.print();
--- dist/string-calculator.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.StringCalculator = void 0;
-class StringCalculator {
-}
-exports.StringCalculator = StringCalculator;
