ffd5f41c545b420ffda5e2e39d41800d8defe319
Radoslav112 <Radi_boy@abv.bg>
2022-07-18 11:18:53 +0300

    project init

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { Person } from "./person";
+import { StringCalculator } from "./string-calculator";
 
 function newFunc() :string {
     return "This is my new function message.";
@@ -7,3 +8,6 @@ function newFunc() :string {
 console.log(newFunc());
 let p = new Person("Ivan","Ivanov",49);
 p.print();
+
+let calculator = new StringCalculator();
+console.log(calculator.summation(""));
--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -1,3 +1,12 @@
 export class StringCalculator{
+
+    public summation(numberList:string): string {
+
+        if(numberList.length=="".length){
+            return "0";
+        }
+
+        return "";
+    }
     
 }
