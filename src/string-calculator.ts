4df70f5e06004485841eff0faf0b7aff8758a8a3
Radoslav112 <Radi_boy@abv.bg>
2022-07-26 10:09:15 +0300

    remove node modules

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -6,9 +6,34 @@ export class StringCalculator{
             return "0";
         }
 
-        numberList.split()
+        let regex = this.createRegex(numberList);
+        let listOfNumbers: string[] = numberList.split(regex);
 
         return "";
     }
+
+    private createRegex(numberList: string): string {
+        let res = ',|\n';
+        if(numberList.includes("//")){
+            let sep = this.getCustomSeparator(numberList);
+            if(
+                sep==="."||sep==="+"||sep==="*"||sep==="?"||
+                sep==="^"||sep==="$"||sep==="("||sep===")"||
+                sep==="{"||sep==="}"||sep==="["||sep==="]"||
+                sep==="|"||sep==="\\"
+            ) {
+                res = "\\".concat(sep);
+            } else {
+                res = sep;
+            }
+        }
+
+        return res;
+    }
     
+    private getCustomSeparator(numberList:string):string {
+        let beginOfSep:number = numberList.indexOf("//")+2;
+        let endOfSep:number = numberList.indexOf("\\n");
+        return numberList.substring(beginOfSep,endOfSep);
+    }
 }
