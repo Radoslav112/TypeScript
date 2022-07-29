0b83efbcb08cfb1ad0bcb76905b86c8ce14e0903
Radoslav112 <Radi_boy@abv.bg>
2022-07-29 11:59:26 +0300

    add error classes and initializing functions

---
+++ src/helpers/last-number-exception.ts
@@ -0,0 +1,8 @@
+export class LastNumberMissedException extends Error {
+    constructor(msg?: string) {
+        super(msg);
+
+        // Set the prototype explicitly.
+        Object.setPrototypeOf(this, new.target.prototype);
+    }
+}
--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -1,3 +1,10 @@
+import { CommaFoundException } from "./helpers/comma-found-exception";
+import { CustomSeparatorFoundException } from "./helpers/custom-separator-found-exception";
+import { EndOfLineFoundException } from "./helpers/eol-found-exception";
+import { LastNumberMissedException } from "./helpers/last-number-exception";
+import { NegativeNumberException } from "./helpers/negative-number-exception";
+import { NumberNotNumericException } from "./helpers/number-not-numberic-exception";
+
 export class StringCalculator{
 
     public summation(numberList:string): string {
@@ -8,6 +15,26 @@ export class StringCalculator{
 
         let regex = this.createRegex(numberList);
         let listOfNumbers: string[] = numberList.split(regex);
+        
+        listOfNumbers.forEach(number => {
+            try{
+                this.checkForErros(numberList,number);
+            } catch(Error) {
+                if(Error instanceof CommaFoundException) {
+
+                } else if(Error instanceof CustomSeparatorFoundException) {
+
+                } else if(Error instanceof EndOfLineFoundException) {
+
+                } else if(Error instanceof LastNumberMissedException) {
+
+                } else if(Error instanceof NegativeNumberException) {
+
+                } else if(Error instanceof NumberNotNumericException) {
+
+                }
+            }
+        });
 
         return "";
     }
@@ -34,6 +61,51 @@ export class StringCalculator{
     private getCustomSeparator(numberList:string):string {
         let beginOfSep:number = numberList.indexOf("//")+2;
         let endOfSep:number = numberList.indexOf("\\n");
-        return numberList.substring(beginOfSep,endOfSep);
+        if(beginOfSep!=-1){
+            return numberList.substring(beginOfSep,endOfSep);
+        }
+        
+        return "";
+    }
+
+    private checkForErros(numberList: string, number: string) {
+        this.isNumberNegative(number);
+        this.isNumberMissed(numberList,number);
+        this.isNumberNumeric(number);
+        this.isLastNumberEmpty(numberList);
+    }
+
+    private isNumberNegative(number: string) {
+        if(number.startsWith("-")) {
+            throw new NegativeNumberException();
+        }        
+    }
+
+    private isNumberMissed(numberList: string, number: string) {
+        const customSep = this.getCustomSeparator(numberList);
+        if(number.trim().length == 0){
+            if(numberList.includes(",,")||numberList.includes("\n,")) {
+                throw new CommaFoundException();
+            }
+            if(numberList.includes(",\n")||numberList.includes("\n\n")) {
+                throw new EndOfLineFoundException();
+            }
+            if(customSep.trim().length!=0 && numberList.includes(customSep.concat(customSep))){
+                throw new CustomSeparatorFoundException();                
+            }
+        }
+    }
+
+    private isNumberNumeric(number: string) {
+        if(!number.match("\\d+?\\.?\\d*")) {
+            throw new NumberNotNumericException();
+        }        
+    }
+
+    private isLastNumberEmpty(numberList: string) {
+        const customSep = this.getCustomSeparator(numberList);
+        if(numberList.endsWith(",")||numberList.endsWith("\n")||(numberList.endsWith(customSep)&&customSep.trim().length!=0)) {
+            throw new LastNumberMissedException();
+        }
     }
 }
